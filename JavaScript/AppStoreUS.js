3c4d6fcfaac33b94d5ed6ca1e7d85e35dfd4b0ce
longthinh <163098554+longthinh@users.noreply.github.com>
2024-05-09 22:53:41 +0700

    Update AppStoreUS.js

--- JavaScript/AppStoreUS.js
+++ JavaScript/AppStoreUS.js
@@ -211,7 +211,7 @@ function emoji(x) {
 function notify(notifys) {
   notifys = notifys.join(`\n`);
   console.log(notifys);
-  $.notify(`${flag(region)}App Store${emoji("emp")}`, ``, notifys);
+  $.notify(`${flag(region)}App Store US${emoji("emp")}`, ``, notifys);
 }
 
 function ENV() {
